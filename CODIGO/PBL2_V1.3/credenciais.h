bc018006abecf4db212f1b68f3f4cecb9d3094dd
Mateus Fernandes Santos <ferrmathh2@gmail.com>
2021-04-28 23:17:03 -0300

    Update credenciais.h

--- CODIGO/PBL2_V1.3/credenciais.h
+++ CODIGO/PBL2_V1.3/credenciais.h
@@ -1,5 +1,5 @@
 #define USER_WIFI "*************"
 #define PASSWORD_WIFI "***********"
 #define ENDPOINT_AWS "*************"
-#define VALOR_TAXA 0.00041544427
-#define VALOR_POTENCIA 50
+#define VALOR_TAXA 0.00041544427 // em Watts*hora
+#define VALOR_POTENCIA 50 //em Watts
